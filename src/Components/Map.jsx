27c9bf433e01026e9e9228bba9c7118ab42211c3
= <=>
2024-05-18 00:19:11 +0200

    Added realtime location using firebase database

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,6 +1,9 @@
-import react from "react"
+import react, { useEffect } from "react"
 import { GoogleMap, useLoadScript, Marker, InfoWindow} from '@react-google-maps/api';
 import { useState } from "react"
+import db from "../Firebase_setup/firebase"
+import { getDatabase, ref, onValue } from "firebase/database";
+
 
 const libraries = ['places'];
 const mapContainerStyle={
@@ -9,18 +12,33 @@ const mapContainerStyle={
 };
 
 const center ={
-    lat: 2,
-    lng: 30,
+    lat: 1.9403,
+    lng: 29.8739,
 }
 
 const Map =()=>{
+    const db = getDatabase();
     const {isLoaded, loadError } = useLoadScript({
         id: "google-map-script",
         googleMapsApiKey: 'AIzaSyDcEMUJb_jK28eKG7xzhm2XSkHshQgCtqk',
         libraries,
     });
 
-  
+  const [ currentLocation, setCurrentLocation ] = useState([]);
+
+  const fetchdata=async()=>{
+    const starCountRef = ref(db,"location/");
+    onValue(starCountRef, (snapshot)=>{
+        const data = snapshot.val();
+        setCurrentLocation(data);
+        console.log(data);
+    });
+  };
+
+  useEffect(()=>{
+    fetchdata();
+  }, [])
+
     return isLoaded ? (
        <div>
         <GoogleMap
